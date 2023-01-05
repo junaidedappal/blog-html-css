b61771378d6ce61e05014f908ee04d2ad3199a33
Junaid Shukapuram <103355607+junaidedappal@users.noreply.github.com>
2023-01-05 22:53:57 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,43 @@
+
+// npm install firebase
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBtC8Q-GJDBKd5Gy9wgKJmqd1iE6FtJO2c",
+  authDomain: "audience0123.firebaseapp.com",
+  projectId: "audience0123",
+  storageBucket: "audience0123.appspot.com",
+  messagingSenderId: "922674067982",
+  appId: "1:922674067982:web:fb012e00acc076311cda5f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// or/another method with script tag
+
+
+//   "<script type="module">""
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBtC8Q-GJDBKd5Gy9wgKJmqd1iE6FtJO2c",
+    authDomain: "audience0123.firebaseapp.com",
+    projectId: "audience0123",
+    storageBucket: "audience0123.appspot.com",
+    messagingSenderId: "922674067982",
+    appId: "1:922674067982:web:fb012e00acc076311cda5f"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
